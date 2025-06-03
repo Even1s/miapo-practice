c56c7c441a33f8daa42119f6b3e8bcc606e93d3f
even1s <eockunev@yandex.ru>
2025-06-03 21:53:37 +0700

    text input on About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <input v-model="value">
+    <p>Your value: {{ value }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const value = ref<string>('');
+</script>
 
 <style>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
+    gap: 16px;
   }
 }
 </style>
