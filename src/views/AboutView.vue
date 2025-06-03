f2fa4a714f9965eacc6c709fad865b533a6a1de0
even1s <eockunev@yandex.ru>
2025-06-03 21:59:17 +0700

    counter on About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="counter">
+      <button @click.prevent="counter++">+</button>
+      <p>{{counter}}</p>
+      <button @click.prevent="counter--">-</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const counter = ref<number>(0);
+</script>
 
 <style>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
+    gap: 16px;
+  }
+}
+.counter {
+  display: flex;
+  gap: 8px;
+  button {
+    width: 32px;
   }
 }
 </style>
